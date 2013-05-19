9c6bca88b2c0935c56c8ed5887fed66fc07269f3
MiguelFernandes <migpfernandes@gmail.com>
2013-05-19 16:55:25 +0100

    Correção esquema de base de dados. Geração de script orientado a SQL Server.

--- bd/Schema.sql
+++ bd/Schema.sql
@@ -1,35 +1,67 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1138\cocoasubrtf510
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural
-
-\f0\fs24 \cf0 /*\
+/*
 Realiza o drop de toads as tabelas\
-*/\
-DROP TABLE IF EXISTS Imagem_Ocorrencia;\
-DROP TABLE IF EXISTS Intervencao;\
-DROP TABLE IF EXISTS Likes;\
-DROP TABLE IF EXISTS Ocorrencia_Tags;\
-DROP TABLE IF EXISTS Classificacao;\
-DROP TABLE IF EXISTS Tags;\
-DROP TABLE IF EXISTS Comentarios;\
-DROP TABLE IF EXISTS Ocorrencia;\
-DROP TABLE IF EXISTS Utilizador;\
-\
-\
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural
-\cf0 /*\
+*/
+ALTER TABLE Comentarios DROP CONSTRAINT FKComentario456261;
+ALTER TABLE Localidades DROP CONSTRAINT FKLocalidade448199;
+ALTER TABLE Comentarios DROP CONSTRAINT FKComentario809369;
+ALTER TABLE Ocorrencia DROP CONSTRAINT FKOcorrencia229570;
+ALTER TABLE Ocorrencia_Tags DROP CONSTRAINT FKOcorrencia671639;
+ALTER TABLE Ocorrencia_Tags DROP CONSTRAINT FKOcorrencia40433;
+ALTER TABLE Likes DROP CONSTRAINT FKLikes389562;
+ALTER TABLE Likes DROP CONSTRAINT FKLikes316397;
+ALTER TABLE Classificacao DROP CONSTRAINT FKClassifica480636;
+ALTER TABLE Amigos DROP CONSTRAINT FKAmigos52968;
+ALTER TABLE Amigos DROP CONSTRAINT FKAmigos52969;
+ALTER TABLE Intervencao DROP CONSTRAINT FKIntervenca847918;
+ALTER TABLE Classificacao DROP CONSTRAINT FKClassifica862287;
+ALTER TABLE Localidades DROP CONSTRAINT FKLocalidade154159;
+ALTER TABLE Imagens DROP CONSTRAINT FKImagens166878;
+ALTER TABLE Imagem_Ocorrencia DROP CONSTRAINT FKImagem_Oco122695;
+ALTER TABLE Imagens DROP CONSTRAINT FKImagens471146;
+DROP TABLE Imagem_Ocorrencia;
+DROP TABLE Intervencao;
+DROP TABLE Amigos;
+DROP TABLE Imagens;
+DROP TABLE Likes;
+DROP TABLE Ocorrencia_Tags;
+DROP TABLE Localidades;
+DROP TABLE Classificacao;
+DROP TABLE Tags;
+DROP TABLE Comentarios;
+DROP TABLE Ocorrencia;
+DROP TABLE Utilizador;
+
+
+/*
 Adiciona as tabelas\
-*/\
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural
-\cf0 CREATE TABLE Imagem_Ocorrencia (Cod_Ocorrencia integer(10) NOT NULL UNIQUE, Cod_Imagem integer(10) NOT NULL UNIQUE, FOREIGN KEY() REFERENCES Ocorrencia());\
-CREATE TABLE Intervencao (Cod_Ocorrencia integer(10) NOT NULL UNIQUE, Cod_Utilizador integer(10) NOT NULL UNIQUE, Cod_Intervencao integer(10) NOT NULL UNIQUE, Descricao varchar(255) NOT NULL, FOREIGN KEY() REFERENCES Ocorrencia());\
-CREATE TABLE Likes (Cod_Utilizador integer(10) NOT NULL UNIQUE, Cod_Ocorrencia integer(10) NOT NULL UNIQUE, FOREIGN KEY() REFERENCES Utilizador(), FOREIGN KEY() REFERENCES Ocorrencia());\
-CREATE TABLE Ocorrencia_Tags (Cod_Ocorrencia integer(10) NOT NULL UNIQUE, Cod_Tag integer(10) NOT NULL UNIQUE, FOREIGN KEY() REFERENCES Tags(), FOREIGN KEY() REFERENCES Ocorrencia());\
-CREATE TABLE Classificacao (Cod_Utilizador integer(10) NOT NULL UNIQUE, Cod_Intervencao integer(10) NOT NULL UNIQUE, Valor integer(10) NOT NULL, FOREIGN KEY() REFERENCES Intervencao(), FOREIGN KEY() REFERENCES Utilizador());\
-CREATE TABLE Tags (Cod_Tag integer(10) NOT NULL UNIQUE, Descricao varchar(255) NOT NULL UNIQUE);\
-CREATE TABLE Comentarios (Cod_Utilizador integer(10) NOT NULL UNIQUE, Cod_Ocorrencia integer(10) NOT NULL UNIQUE, Conteudo integer(10) NOT NULL, Cod_Imagem varchar(255) UNIQUE, Cod_Comentario integer(10) NOT NULL UNIQUE, FOREIGN KEY() REFERENCES Utilizador(), FOREIGN KEY() REFERENCES Ocorrencia());\
-CREATE TABLE Ocorrencia (Cod_Utilizador integer(10) NOT NULL UNIQUE, Cod_Ocorrencia integer(10) NOT NULL UNIQUE, Descricao integer(10) NOT NULL, Estado varchar(255) NOT NULL, FOREIGN KEY() REFERENCES Utilizador());\
-CREATE TABLE Utilizador (Cod_Utilizador integer(10) NOT NULL UNIQUE, Nome varchar(255) NOT NULL, Morada varchar(255) NOT NULL, Tipo char(1) NOT NULL, Login varchar(255) NOT NULL, Password varchar(255) NOT NULL);\
-}
+*/
+
+CREATE TABLE Imagem_Ocorrencia (OcorrenciaID int NOT NULL UNIQUE, ImagemID int NOT NULL UNIQUE);
+CREATE TABLE Intervencao (OcorrenciaID int NOT NULL UNIQUE, UtilizadorID int NOT NULL UNIQUE, IntervencaoID int NOT NULL UNIQUE, Descricao varchar(255) NOT NULL);
+CREATE TABLE Amigos (UtilizadorID int NULL, AmigoID int NULL);
+CREATE TABLE Imagens (ImagemID int NOT NULL UNIQUE, Imagem binary(1000) NOT NULL);
+CREATE TABLE Likes (UtilizadorID int NOT NULL UNIQUE, OcorrenciaID int NOT NULL UNIQUE);
+CREATE TABLE Ocorrencia_Tags (OcorrenciaID int NOT NULL UNIQUE, TagID int NOT NULL UNIQUE);
+CREATE TABLE Localidades (LocalidadeID int NOT NULL UNIQUE, Nome varchar(400) NULL, Coordenadas varchar(255) NULL);
+CREATE TABLE Classificacao (UtilizadorID int NOT NULL UNIQUE, IntervencaoID int NOT NULL UNIQUE, Valor int NOT NULL);
+CREATE TABLE Tags (TagID int NOT NULL UNIQUE, Descricao varchar(255) NOT NULL UNIQUE);
+CREATE TABLE Comentarios (UtilizadorID int NOT NULL UNIQUE, OcorrenciaID int NOT NULL UNIQUE, Conteudo int NOT NULL, ImagemID varchar(255) NULL UNIQUE, ComentarioID int NOT NULL UNIQUE);
+CREATE TABLE Ocorrencia (UtilizadorID int NOT NULL UNIQUE, OcorrenciaID int NOT NULL UNIQUE, Descricao int NOT NULL, Estado varchar(255) NOT NULL, Morada varchar(255) NULL, Coordenadas varchar(255) NULL);
+CREATE TABLE Utilizador (UtilizadorID int NOT NULL UNIQUE, Nome varchar(255) NOT NULL, Morada varchar(255) NOT NULL, Tipo char(1) NOT NULL, Login varchar(255) NOT NULL, Password varchar(255) NOT NULL, LocalidadeID int NULL);
+ALTER TABLE Comentarios ADD CONSTRAINT FKComentario456261 FOREIGN KEY () REFERENCES Ocorrencia ();
+ALTER TABLE Localidades ADD CONSTRAINT FKLocalidade448199 FOREIGN KEY () REFERENCES Ocorrencia ();
+ALTER TABLE Comentarios ADD CONSTRAINT FKComentario809369 FOREIGN KEY () REFERENCES Utilizador ();
+ALTER TABLE Ocorrencia ADD CONSTRAINT FKOcorrencia229570 FOREIGN KEY () REFERENCES Utilizador ();
+ALTER TABLE Ocorrencia_Tags ADD CONSTRAINT FKOcorrencia671639 FOREIGN KEY () REFERENCES Ocorrencia ();
+ALTER TABLE Ocorrencia_Tags ADD CONSTRAINT FKOcorrencia40433 FOREIGN KEY () REFERENCES Tags ();
+ALTER TABLE Likes ADD CONSTRAINT FKLikes389562 FOREIGN KEY () REFERENCES Ocorrencia ();
+ALTER TABLE Likes ADD CONSTRAINT FKLikes316397 FOREIGN KEY () REFERENCES Utilizador ();
+ALTER TABLE Classificacao ADD CONSTRAINT FKClassifica480636 FOREIGN KEY () REFERENCES Utilizador ();
+ALTER TABLE Amigos ADD CONSTRAINT FKAmigos52968 FOREIGN KEY () REFERENCES Utilizador ();
+ALTER TABLE Amigos ADD CONSTRAINT FKAmigos52969 FOREIGN KEY () REFERENCES Utilizador ();
+ALTER TABLE Intervencao ADD CONSTRAINT FKIntervenca847918 FOREIGN KEY () REFERENCES Ocorrencia ();
+ALTER TABLE Classificacao ADD CONSTRAINT FKClassifica862287 FOREIGN KEY () REFERENCES Intervencao ();
+ALTER TABLE Localidades ADD CONSTRAINT FKLocalidade154159 FOREIGN KEY () REFERENCES Utilizador ();
+ALTER TABLE Imagens ADD CONSTRAINT FKImagens166878 FOREIGN KEY () REFERENCES Comentarios ();
+ALTER TABLE Imagem_Ocorrencia ADD CONSTRAINT FKImagem_Oco122695 FOREIGN KEY () REFERENCES Ocorrencia ();
+ALTER TABLE Imagens ADD CONSTRAINT FKImagens471146 FOREIGN KEY () REFERENCES Imagem_Ocorrencia ();
