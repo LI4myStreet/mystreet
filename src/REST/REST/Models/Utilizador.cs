e557cedfca442c883799141a861ae0757ea759ae
Bruno Matos <bruno.matos@gmail.com>
2013-06-03 19:46:21 +0100

    REST: Eliminação de classes repetidas.  * Eliminação da classe User que é uma repetição da classe Utilizador.

--- src/REST/REST/Models/Utilizador.cs
+++ src/REST/REST/Models/Utilizador.cs
@@ -20,7 +20,65 @@ namespace REST.Models
         public string Username { get; set; }
         [Required, MaxLength(255)]
         public string Password { get; set; }
-        [Required, MaxLength(1), DefaultValue("U")]
-        public string Tipo { get; set; }
+        [Required, DefaultValue(-1)]
+        public int Tipo { get; set; }
+
+         public Utilizador()
+        {
+            Id = -1;
+            Nome = "";
+            Morada = "";
+            Tipo = -1;
+            Username = "";
+            Password = "";
+        }
+
+        public Utilizador(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+            Nome = "";
+            Tipo = 0;
+            Morada = "";
+        }
+
+        public Utilizador(int id,string nome, string morada, string username, string password)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Morada = morada;
+            this.Username = username;
+            this.Password = password;
+            Tipo = 0;
+        }
+
+        public Utilizador(Utilizador u)
+        {
+            this.Id = u.Id;
+            this.Nome = u.Nome;
+            this.Morada = u.Morada;
+            this.Tipo = u.Tipo;
+            this.Username = u.Username;
+            this.Password = u.Password;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("User: {0}\nNome: {1}\nMorada: {2}\n",Username,Nome,Morada);
+        }
+
+        public bool Compara(object obj)
+        {
+            if (obj == null) return false;
+            Utilizador u = obj as Utilizador;
+            if ((object)u == null) return false;
+            if (Id == u.Id && Nome.Equals(u.Nome) && Morada.Equals(u.Morada) && Tipo == u.Tipo && Username.Equals(u.Username) && Password.Equals(u.Password)) return true;
+            return false;
+        }
+
+        public Utilizador Clone()
+        {
+            return new Utilizador(this);
+        }
     }
 }
